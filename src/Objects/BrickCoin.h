62a7cf77b84c68ce9f558b3ef8748d2c3c90f96b
Mesutcan Kurt <mesutcank@gmail.com>
2018-11-06 23:13:39 +0300

    * fix type conversion warning

--- src/Objects/BrickCoin.h
+++ src/Objects/BrickCoin.h
@@ -43,7 +43,7 @@ public:
     }
 
     ~BrickCoin() {
-        for (int i = 0; i < texture.size(); i++) {
+        for (size_t i = 0; i < texture.size(); i++) {
             SDL_DestroyTexture(texture[i]);
         }
 
